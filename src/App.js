2c1be36e78b8eb7a74fc195c36e700f0fed119cc
Fabien Chareun <cacrfabfr@gmail.com>
2022-09-19 10:46:44 +0200

    clean up and setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [isVisible, setIsVisible] = useState(false);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button
+        onClick={() => {
+          setIsVisible((v) => !v);
+        }}
+      >
+        {isVisible ? "unmount" : "mount"}
+      </button>
+      <div className="container">
+        {isVisible ? <div className="item" /> : ""}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
